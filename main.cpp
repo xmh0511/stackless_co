40c27b1d3717b3d62c5622f48820b1a9ad154237
xmh0511 <970252187@qq.com>
2019-12-12 14:20:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include "stackless_co.hpp"
+#include <thread>
 auto test() {
 	int a = 0;
 	int b = 0;
-	CO_BEGIN(a, b)
-		std::cout << "begin0 a = " << a << std::endl;
+	CO_BEGIN(int,a, b)
+	std::cout << "begin0 a = " << a << std::endl;
+	std::thread t0([&promise = shared(),this]() {
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		promise.resume(1024);
+	});
+	t0.detach();
 	CO_yiled()
-		std::cout << "resume0" << b << std::endl;
+     //int abb = 111;
+	  std::cout << "resume0 " << get()<< std::endl;
 	CO_END()
 }
 auto show() {
-	CO_BEGIN()
+	CO_BEGIN(void)
 	{
 		std::cout << "begin 1" << std::endl;
 	}
@@ -26,10 +33,9 @@ auto show() {
 }
 int main() {
 	auto co = test();
-	auto c02 = show();
+	auto co1 = show();
 	co();
-	c02();
-	co();
-	c02();
-	//c02();
+	co1();
+	co1();
+	std::getchar();
 }
