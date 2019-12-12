3fe1c62b9809c87a796e27cdf9e3b1efd9d89c55
xmh0511 <970252187@qq.com>
2019-12-12 15:15:24 +0800

    Update stackless_co.hpp

--- stackless_co.hpp
+++ stackless_co.hpp
@@ -18,45 +18,58 @@
 
 #define FOR_ACTION(Func,...) macro_expand(concat_ab(Func,args_number(__VA_ARGS__)) (__VA_ARGS__))
 
+
+#define CAtorType0()
+#define CAtorType1(element)  using element##_type = decltype(element);
+#define CAtorType2(element,...) using element##_type = decltype(element); macro_expand(CAtorType1(__VA_ARGS__))
+#define CAtorType3(element,...) using element##_type = decltype(element); macro_expand(CAtorType2(__VA_ARGS__))
+#define CAtorType4(element,...) using element##_type = decltype(element); macro_expand(CAtorType3(__VA_ARGS__))
+#define CAtorType5(element,...) using element##_type = decltype(element); macro_expand(CAtorType4(__VA_ARGS__))
+#define CAtorType6(element,...) using element##_type = decltype(element); macro_expand(CAtorType5(__VA_ARGS__))
+#define CAtorType7(element,...) using element##_type = decltype(element); macro_expand(CAtorType6(__VA_ARGS__))
+#define CAtorType8(element,...) using element##_type = decltype(element); macro_expand(CAtorType7(__VA_ARGS__))
+#define CAtorType9(element,...) using element##_type = decltype(element); macro_expand(CAtorType8(__VA_ARGS__))
+#define CAtorType10(element,...) using element##_type = decltype(element);  macro_expand(CAtorType9(__VA_ARGS__))
+
 #define CAtor0()
-#define CAtor1(element) decltype(element) element##_
-#define CAtor2(element,...) decltype(element) element##_, macro_expand(CAtor1(__VA_ARGS__))
-#define CAtor3(element,...) decltype(element) element##_, macro_expand(CAtor2(__VA_ARGS__))
-#define CAtor4(element,...) decltype(element) element##_, macro_expand(CAtor3(__VA_ARGS__))
-#define CAtor5(element,...) decltype(element) element##_, macro_expand(CAtor4(__VA_ARGS__))
-#define CAtor6(element,...) decltype(element) element##_, macro_expand(CAtor5(__VA_ARGS__))
-#define CAtor7(element,...) decltype(element) element##_, macro_expand(CAtor6(__VA_ARGS__))
-#define CAtor8(element,...) decltype(element) element##_, macro_expand(CAtor7(__VA_ARGS__))
-#define CAtor9(element,...) decltype(element) element##_, macro_expand(CAtor8(__VA_ARGS__))
-#define CAtor10(element,...) decltype(element) element##_, macro_expand(CAtor9(__VA_ARGS__))
+#define CAtor1(element) element##_type element##_
+#define CAtor2(element,...) element##_type element##_, macro_expand(CAtor1(__VA_ARGS__))
+#define CAtor3(element,...) element##_type element##_, macro_expand(CAtor2(__VA_ARGS__))
+#define CAtor4(element,...) element##_type element##_, macro_expand(CAtor3(__VA_ARGS__))
+#define CAtor5(element,...) element##_type element##_, macro_expand(CAtor4(__VA_ARGS__))
+#define CAtor6(element,...) element##_type element##_, macro_expand(CAtor5(__VA_ARGS__))
+#define CAtor7(element,...) element##_type element##_, macro_expand(CAtor6(__VA_ARGS__))
+#define CAtor8(element,...) element##_type element##_, macro_expand(CAtor7(__VA_ARGS__))
+#define CAtor9(element,...) element##_type element##_, macro_expand(CAtor8(__VA_ARGS__))
+#define CAtor10(element,...) element##_type element##_, macro_expand(CAtor9(__VA_ARGS__))
 
 
 
 #define ActorInit0()
-#define ActorInit1(element) element##(element##_)
-#define ActorInit2(element,...) element##(element##_), macro_expand(ActorInit1(__VA_ARGS__))
-#define ActorInit3(element,...) element##(element##_), macro_expand(ActorInit2(__VA_ARGS__))
-#define ActorInit4(element,...) element##(element##_), macro_expand(ActorInit3(__VA_ARGS__))
-#define ActorInit5(element,...) element##(element##_), macro_expand(ActorInit4(__VA_ARGS__))
-#define ActorInit6(element,...) element##(element##_), macro_expand(ActorInit5(__VA_ARGS__))
-#define ActorInit7(element,...) element##(element##_), macro_expand(ActorInit6(__VA_ARGS__))
-#define ActorInit8(element,...) element##(element##_), macro_expand(ActorInit7(__VA_ARGS__))
-#define ActorInit9(element,...) element##(element##_), macro_expand(ActorInit8(__VA_ARGS__))
-#define ActorInit10(element,...) element##(element##_), macro_expand(ActorInit9(__VA_ARGS__))
+#define ActorInit1(element) element(element##_)
+#define ActorInit2(element,...) element(element##_), macro_expand(ActorInit1(__VA_ARGS__))
+#define ActorInit3(element,...) element(element##_), macro_expand(ActorInit2(__VA_ARGS__))
+#define ActorInit4(element,...) element(element##_), macro_expand(ActorInit3(__VA_ARGS__))
+#define ActorInit5(element,...) element(element##_), macro_expand(ActorInit4(__VA_ARGS__))
+#define ActorInit6(element,...) element(element##_), macro_expand(ActorInit5(__VA_ARGS__))
+#define ActorInit7(element,...) element(element##_), macro_expand(ActorInit6(__VA_ARGS__))
+#define ActorInit8(element,...) element(element##_), macro_expand(ActorInit7(__VA_ARGS__))
+#define ActorInit9(element,...) element(element##_), macro_expand(ActorInit8(__VA_ARGS__))
+#define ActorInit10(element,...) element(element##_), macro_expand(ActorInit9(__VA_ARGS__))
 
 
 
 #define ActorDeclareVar0()
-#define ActorDeclareVar1(element) decltype(element) element;
-#define ActorDeclareVar2(element,...) decltype(element) element; macro_expand(ActorDeclareVar1(__VA_ARGS__))
-#define ActorDeclareVar3(element,...) decltype(element) element; macro_expand(ActorDeclareVar2(__VA_ARGS__))
-#define ActorDeclareVar4(element,...) decltype(element) element; macro_expand(ActorDeclareVar3(__VA_ARGS__))
-#define ActorDeclareVar5(element,...) decltype(element) element; macro_expand(ActorDeclareVar4(__VA_ARGS__))
-#define ActorDeclareVar6(element,...) decltype(element) element; macro_expand(ActorDeclareVar5(__VA_ARGS__))
-#define ActorDeclareVar7(element,...) decltype(element) element; macro_expand(ActorDeclareVar6(__VA_ARGS__))
-#define ActorDeclareVar8(element,...) decltype(element) element; macro_expand(ActorDeclareVar9(__VA_ARGS__))
-#define ActorDeclareVar9(element,...) decltype(element) element; macro_expand(ActorDeclareVar8(__VA_ARGS__))
-#define ActorDeclareVar10(element,...) decltype(element) element; macro_expand(ActorDeclareVar9(__VA_ARGS__))
+#define ActorDeclareVar1(element) element##_type element;
+#define ActorDeclareVar2(element,...) element##_type element; macro_expand(ActorDeclareVar1(__VA_ARGS__))
+#define ActorDeclareVar3(element,...) element##_type element; macro_expand(ActorDeclareVar2(__VA_ARGS__))
+#define ActorDeclareVar4(element,...) element##_type element; macro_expand(ActorDeclareVar3(__VA_ARGS__))
+#define ActorDeclareVar5(element,...) element##_type element; macro_expand(ActorDeclareVar4(__VA_ARGS__))
+#define ActorDeclareVar6(element,...) element##_type element; macro_expand(ActorDeclareVar5(__VA_ARGS__))
+#define ActorDeclareVar7(element,...) element##_type element; macro_expand(ActorDeclareVar6(__VA_ARGS__))
+#define ActorDeclareVar8(element,...) element##_type element; macro_expand(ActorDeclareVar9(__VA_ARGS__))
+#define ActorDeclareVar9(element,...) element##_type element; macro_expand(ActorDeclareVar8(__VA_ARGS__))
+#define ActorDeclareVar10(element,...) element##_type element; macro_expand(ActorDeclareVar9(__VA_ARGS__))
 
 #define ActDot0(...) 
 #define ActDot1(...) :
@@ -88,6 +101,7 @@ auto expand_tuple(Tuple&& tuple, std::index_sequence<Index...>) {
 }
 #define CO_BEGIN(Ret,...)\
 \
+macro_expand(FOR_ACTION(CAtorType,__VA_ARGS__))\
 auto save_stack_ = std::make_tuple(__VA_ARGS__);\
 \
 class co_type{ \
@@ -128,4 +142,4 @@ switch(state_){case 0:{
 
 #define CO_yiled() state_= __LINE__;}break;case __LINE__:{\
 
-#define CO_END()  state_ = 0;}}return *this;}}; return expand_tuple<co_type>(save_stack_,std::make_index_sequence<std::tuple_size_v<decltype(save_stack_)>>{});
+#define CO_END()  state_ = 0;}}return *this;}}; return expand_tuple<co_type>(save_stack_,std::make_index_sequence<std::tuple_size<decltype(save_stack_)>::value>{});
